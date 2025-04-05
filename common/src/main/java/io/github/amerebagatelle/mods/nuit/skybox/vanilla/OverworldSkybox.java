adffa0be94968a55232f3c40277262d248b22017
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2025-04-05 10:08:56 -0600

    change: Fix sunrise/sunset flickering

--- common/src/main/java/io/github/amerebagatelle/mods/nuit/skybox/vanilla/OverworldSkybox.java
+++ common/src/main/java/io/github/amerebagatelle/mods/nuit/skybox/vanilla/OverworldSkybox.java
@@ -13,15 +13,9 @@ import io.github.amerebagatelle.mods.nuit.skybox.AbstractSkybox;
 import io.github.amerebagatelle.mods.nuit.skybox.decorations.DecorationBox;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.FogParameters;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.SkyRenderer;
+import net.minecraft.client.renderer.*;
 import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
-
-import java.util.Objects;
 
 public class OverworldSkybox extends AbstractSkybox {
     public static Codec<OverworldSkybox> CODEC = RecordCodecBuilder.create(instance -> instance.group(
@@ -36,28 +30,29 @@ public class OverworldSkybox extends AbstractSkybox {
     @Override
     public void render(SkyRendererAccessor skyRendererAccess, PoseStack poseStack, float tickDelta, Camera camera, MultiBufferSource.BufferSource bufferSource, FogParameters fogParameters) {
         RenderSystem.setShaderFog(fogParameters);
-        Entity entity = camera.getEntity();
-        ClientLevel world = Objects.requireNonNull((ClientLevel) entity.level());
-        int skyColor = world.getSkyColor(camera.getPosition(), tickDelta);
+        ClientLevel level = (ClientLevel) camera.getEntity().level();
+        float sunAngle = level.getSunAngle(tickDelta);
+        float timeOfDay = level.getTimeOfDay(tickDelta);
+        level.getRainLevel(tickDelta);
+        int sunriseOrSunsetColor = level.effects().getSunriseOrSunsetColor(timeOfDay);
+        int skyColor = level.getSkyColor(camera.getPosition(), tickDelta);
 
         // Light Sky
         RenderSystem.setShaderColor(ARGB.redFloat(skyColor), ARGB.greenFloat(skyColor), ARGB.blueFloat(skyColor), this.alpha);
         skyRendererAccess.getTopSkyBuffer().drawWithRenderType(RenderType.sky());
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-        int sunriseOrSunsetColor = world.effects().getSunriseOrSunsetColor(tickDelta);
-        if (world.effects().isSunriseOrSunset(world.getTimeOfDay(tickDelta))) {
-            float skyAngle = world.getSunAngle(tickDelta);
+        if (level.effects().isSunriseOrSunset(timeOfDay)) {
             if (SkyboxManager.getInstance().isEnabled() && NuitApi.getInstance().getActiveSkyboxes().stream().anyMatch(skybox -> skybox instanceof DecorationBox decorationBox && decorationBox.getProperties().rotation().skyboxRotation())) {
-                skyAngle = Mth.positiveModulo(world.getDayTime() / 24000F + 0.75F, 1);
+                sunAngle = Mth.positiveModulo(level.getDayTime() / 24000F + 0.75F, 1);
             }
-            ((SkyRenderer) skyRendererAccess).renderSunriseAndSunset(poseStack, bufferSource, skyAngle, sunriseOrSunsetColor);
+            ((SkyRenderer) skyRendererAccess).renderSunriseAndSunset(poseStack, bufferSource, sunAngle, sunriseOrSunsetColor);
         }
 
         bufferSource.endBatch();
 
         // Dark Sky
-        double eyeHeight = entity.getEyePosition(tickDelta).y - world.getLevelData().getHorizonHeight(world);
+        double eyeHeight = camera.getEntity().getEyePosition(tickDelta).y - level.getLevelData().getHorizonHeight(level);
         if (eyeHeight < 0.0) {
             ((SkyRenderer) skyRendererAccess).renderDarkDisc(poseStack);
         }
