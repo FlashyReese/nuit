5f2f9e2ee6d9a67dd976f2203330e0340e31950e
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2025-04-09 23:18:24 -0600

    change: Update `overworld` rendering methods

--- common/src/main/java/io/github/amerebagatelle/mods/nuit/skybox/vanilla/OverworldSkybox.java
+++ common/src/main/java/io/github/amerebagatelle/mods/nuit/skybox/vanilla/OverworldSkybox.java
@@ -2,6 +2,8 @@ package io.github.amerebagatelle.mods.nuit.skybox.vanilla;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.amerebagatelle.mods.nuit.SkyboxManager;
@@ -13,9 +15,12 @@ import io.github.amerebagatelle.mods.nuit.skybox.AbstractSkybox;
 import io.github.amerebagatelle.mods.nuit.skybox.decorations.DecorationBox;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.FogParameters;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
+import org.joml.Matrix4f;
 
 public class OverworldSkybox extends AbstractSkybox {
     public static Codec<OverworldSkybox> CODEC = RecordCodecBuilder.create(instance -> instance.group(
@@ -33,20 +38,17 @@ public class OverworldSkybox extends AbstractSkybox {
         ClientLevel level = (ClientLevel) camera.getEntity().level();
         float sunAngle = level.getSunAngle(tickDelta);
         float timeOfDay = level.getTimeOfDay(tickDelta);
-        level.getRainLevel(tickDelta);
         int sunriseOrSunsetColor = level.effects().getSunriseOrSunsetColor(timeOfDay);
         int skyColor = level.getSkyColor(camera.getPosition(), tickDelta);
 
         // Light Sky
-        RenderSystem.setShaderColor(ARGB.redFloat(skyColor), ARGB.greenFloat(skyColor), ARGB.blueFloat(skyColor), this.alpha);
-        skyRendererAccess.getTopSkyBuffer().drawWithRenderType(RenderType.sky());
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        this.renderLightSky(skyColor, skyRendererAccess);
 
         if (level.effects().isSunriseOrSunset(timeOfDay)) {
             if (SkyboxManager.getInstance().isEnabled() && NuitApi.getInstance().getActiveSkyboxes().stream().anyMatch(skybox -> skybox instanceof DecorationBox decorationBox && decorationBox.getProperties().rotation().skyboxRotation())) {
                 sunAngle = Mth.positiveModulo(level.getDayTime() / 24000F + 0.75F, 1);
             }
-            ((SkyRenderer) skyRendererAccess).renderSunriseAndSunset(poseStack, bufferSource, sunAngle, sunriseOrSunsetColor);
+            this.renderSunriseAndSunset(poseStack, bufferSource, sunAngle, sunriseOrSunsetColor);
         }
 
         bufferSource.endBatch();
@@ -54,7 +56,54 @@ public class OverworldSkybox extends AbstractSkybox {
         // Dark Sky
         double eyeHeight = camera.getEntity().getEyePosition(tickDelta).y - level.getLevelData().getHorizonHeight(level);
         if (eyeHeight < 0.0) {
-            ((SkyRenderer) skyRendererAccess).renderDarkDisc(poseStack);
+            this.renderDarkSky(poseStack, skyRendererAccess);
         }
+    }
+
+    private void renderSunriseAndSunset(PoseStack poseStack, MultiBufferSource.BufferSource bufferSource, float sunAngle, int sunriseOrSunsetColor) {
+        poseStack.pushPose();
+
+        // Rotate to orient the effect properly
+        poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
+        float zRotation = Mth.sin(sunAngle) < 0.0F ? 180.0F : 0.0F;
+        poseStack.mulPose(Axis.ZP.rotationDegrees(zRotation));
+        poseStack.mulPose(Axis.ZP.rotationDegrees(90.0F));
+
+        Matrix4f transformationMatrix = poseStack.last().pose();
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.sunriseSunset());
+
+        sunriseOrSunsetColor = ARGB.color(sunriseOrSunsetColor, (int) (this.alpha * 255));
+
+        float alpha = ARGB.alphaFloat(sunriseOrSunsetColor);
+        vertexConsumer.addVertex(transformationMatrix, 0.0F, 100.0F, 0.0F).setColor(sunriseOrSunsetColor);
+
+        int transparentColor = ARGB.transparent(sunriseOrSunsetColor);
+
+        for (int i = 0; i <= 16; i++) {
+            float angleRadians = (float) i * ((float) Math.PI * 2F) / 16.0F;
+            float x = Mth.sin(angleRadians);
+            float y = Mth.cos(angleRadians);
+            float z = -y * 40.0F * alpha;
+
+            vertexConsumer.addVertex(transformationMatrix, x * 120.0F, y * 120.0F, z).setColor(transparentColor);
+        }
+
+        poseStack.popPose();
+    }
+
+
+    private void renderLightSky(int skyColor, SkyRendererAccessor skyRendererAccessor) {
+        RenderSystem.setShaderColor(ARGB.redFloat(skyColor), ARGB.greenFloat(skyColor), ARGB.blueFloat(skyColor), this.alpha);
+        skyRendererAccessor.getTopSkyBuffer().drawWithRenderType(RenderType.sky());
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    private void renderDarkSky(PoseStack poseStack, SkyRendererAccessor skyRendererAccessor) {
+        RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, this.alpha);
+        poseStack.pushPose();
+        poseStack.translate(0.0F, 12.0F, 0.0F);
+        skyRendererAccessor.getBottomSkyBuffer().drawWithRenderType(RenderType.sky());
+        poseStack.popPose();
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
