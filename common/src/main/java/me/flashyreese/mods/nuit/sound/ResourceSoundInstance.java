AMereBagatelle/fabricskyboxes

Add sound support to skyboxes
Resource packs can now add sounds through `properties.sound`, using the skybox’s existing conditions, fades, and clock.

Sounds can loop, fade in and out, or play after a delay. Setting `clipToFade` to `false` lets a non-looping sound finish even after the skybox fades away. Files use Ogg Vorbis and don’t need an entry in `sounds.json`.

Players control the volume through Minecraft’s Ambient/Environment and Master Volume sliders. Reloading resources, disabling Nuit, leaving the world, or changing dimensions stops playback.

Closes #95.

common/src/main/java/me/flashyreese/mods/nuit/sound/ResourceSoundInstance.java
@@ -1,0 +1,75 @@
+package me.flashyreese.mods.nuit.sound;
+
+import me.flashyreese.mods.nuit.components.SoundSettings;
+import net.minecraft.client.resources.sounds.AbstractSoundInstance;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.resources.sounds.TickableSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.client.sounds.WeighedSoundEvents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.valueproviders.ConstantFloat;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * A non-positional Ogg resource using Minecraft's Ambient/Environment volume control.
+ */
+public final class ResourceSoundInstance extends AbstractSoundInstance implements TickableSoundInstance {
+    private final WeighedSoundEvents event;
+    private boolean stopped;
+
+    public ResourceSoundInstance(Identifier file) {
+        this(new SoundSettings(file), 1.0F);
+    }
+
+    public ResourceSoundInstance(SoundSettings settings, float volume) {
+        super(settings.file(), SoundSource.AMBIENT, SoundInstance.createUnseededRandom());
+        this.setVolume(volume);
+        this.pitch = 1.0F;
+        this.looping = settings.loop();
+        this.delay = 0;
+        this.relative = true;
+        this.attenuation = Attenuation.NONE;
+        this.sound = new Sound(
+                Sound.SOUND_LISTER.fileToId(settings.file()),
+                ConstantFloat.of(1.0F),
+                ConstantFloat.of(1.0F),
+                1,
+                Sound.Type.FILE,
+                true,
+                false,
+                16
+        );
+        this.event = new WeighedSoundEvents(this.identifier, null);
+        this.event.addSound(this.sound);
+    }
+
+    @Override
+    public WeighedSoundEvents resolve(@NonNull SoundManager soundManager) {
+        return this.event;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = Float.isFinite(volume) ? Math.clamp(volume, 0.0F, 1.0F) : 0.0F;
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public boolean canStartSilent() {
+        return true;
+    }
+
+    @Override
+    public boolean isStopped() {
+        return this.stopped;
+    }
+
+    public void stopPlayback() {
+        this.stopped = true;
+        this.looping = false;
+    }
+}

FlashyReese/nuit

common/src/main/java/me/flashyreese/mods/nuit/sound/ResourceSoundInstance.java
@@ -1,0 +1,74 @@
+package me.flashyreese.mods.nuit.sound;
+
+import me.flashyreese.mods.nuit.components.SoundSettings;
+import net.minecraft.client.resources.sounds.AbstractSoundInstance;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.resources.sounds.TickableSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.client.sounds.WeighedSoundEvents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.valueproviders.ConstantFloat;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * A non-positional Ogg resource using Minecraft's Ambient/Environment volume control.
+ */
+public final class ResourceSoundInstance extends AbstractSoundInstance implements TickableSoundInstance {
+    private boolean stopped;
+
+    public ResourceSoundInstance(Identifier file) {
+        this(new SoundSettings(file), 1.0F);
+    }
+
+    public ResourceSoundInstance(SoundSettings settings, float volume) {
+        super(settings.file(), SoundSource.AMBIENT, SoundInstance.createUnseededRandom());
+        this.setVolume(volume);
+        this.pitch = 1.0F;
+        this.looping = settings.loop();
+        this.delay = 0;
+        this.relative = true;
+        this.attenuation = Attenuation.NONE;
+        this.sound = new Sound(
+                Sound.SOUND_LISTER.fileToId(settings.file()),
+                ConstantFloat.of(1.0F),
+                ConstantFloat.of(1.0F),
+                1,
+                Sound.Type.FILE,
+                true,
+                false,
+                16
+        );
+        this.soundEvent = new WeighedSoundEvents(this.identifier, null);
+        this.soundEvent.addSound(this.sound);
+    }
+
+    @Override
+    public WeighedSoundEvents getOrResolve(@NonNull SoundManager soundManager) {
+        return this.soundEvent;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = Float.isFinite(volume) ? Math.clamp(volume, 0.0F, 1.0F) : 0.0F;
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public boolean canStartSilent() {
+        return true;
+    }
+
+    @Override
+    public boolean isStopped() {
+        return this.stopped;
+    }
+
+    public void stopPlayback() {
+        this.stopped = true;
+        this.looping = false;
+    }
+}